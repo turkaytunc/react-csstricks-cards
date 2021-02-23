6d44a8165db455457bc02a3bdbcf99fcdf5095c5
turkaytunc <turkaytunc00@gmail.com>
2021-02-23 19:25:37 +0300

    update: layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Card from './components/card/Card';
 function App() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="app">
-      {arr.map((_, i) => (
-        <Card key={i} pubDate={arr[Math.floor(Math.random() * 15)]} />
-      ))}
+    <div className="app-container">
+      <div className="app">
+        {arr.map((_, i) => (
+          <Card key={i} pubDate={arr[Math.floor(Math.random() * 15)]} />
+        ))}
+      </div>
     </div>
   );
 }
