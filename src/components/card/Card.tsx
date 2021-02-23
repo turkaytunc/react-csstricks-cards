7533ddb1b26c83764b80aece3f30f75c25882302
turkaytunc <turkaytunc00@gmail.com>
2021-02-23 20:23:15 +0300

    add: fonts

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,8 +4,11 @@ import './card.scss';
 const Card = ({ pubDate }: { pubDate: number }) => {
   return (
     <article className="card-container">
-      <h3>Article on March {pubDate} 2021</h3>
-      <p>This is a Card component</p>
+      <p className="card-header">Article on March {pubDate} 2021</p>
+      <p className="card-content">Lorem ipsum dolor sit amet.</p>
+      <p className="card-content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, quibusdam!
+      </p>
     </article>
   );
 };
