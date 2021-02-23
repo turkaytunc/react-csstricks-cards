d0b4608e19b32fe4264dac91137eae32d88c381f
turkaytunc <turkaytunc00@gmail.com>
2021-02-23 18:35:20 +0300

    add: placeholder data to cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './app.scss';
 import Card from './components/card/Card';
 
 function App() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="app">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => (
-        <Card key={i} />
+      {arr.map((_, i) => (
+        <Card key={i} pubDate={arr[Math.floor(Math.random() * 15)]} />
       ))}
     </div>
   );
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './card.scss';
 
-const Card = () => {
-  return <article className="card-container">This is a Card component</article>;
+const Card = ({ pubDate }: { pubDate: number }) => {
+  return (
+    <article className="card-container">
+      <h1>Article on March {pubDate} 2021</h1>
+      <p>This is a Card component</p>
+    </article>
+  );
 };
 
 export default Card;
