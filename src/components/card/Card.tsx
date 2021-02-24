aa1ecff5bd254d0930ec5d9e792f653e2751c397
turkaytunc <turkaytunc00@gmail.com>
2021-02-24 11:02:24 +0300

    add: inject css variable from inline css

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './card.scss';
 
 const Card = ({ pubDate }: { pubDate: number }) => {
+  const textColor = 'green';
   return (
     <article className="card-container">
       <p className="card-header">Article on March {pubDate} 2021</p>
       <p className="card-content">Lorem ipsum dolor sit amet.</p>
-      <p className="card-content">
+      <p className="card-content variable-inject" style={{ ['--text-color' as any]: `${textColor}` }}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, quibusdam!
       </p>
     </article>
